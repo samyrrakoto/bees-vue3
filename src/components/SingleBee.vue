105b4d2df3e34872d53fd69194f7016fd331cb96
Samyr Rakoto <samyr.rakoto@gmail.com>
2022-11-08 20:38:29 +0100

    Even moar design

--- src/components/SingleBee.vue
+++ src/components/SingleBee.vue
@@ -7,7 +7,7 @@
         <span>{{ role }}</span> -
         <span>{{ lp }}</span> /
         <span>{{ hp }}</span>
-        <span v-if="lp == 0">💀</span>
+        <span v-if="lp == 0"> 💀</span>
     </div>
 </template>
 
@@ -46,27 +46,100 @@ export default defineComponent({
     margin-top: 10px;
     padding: 10px;
     color: white;
-    border: 1px solid #FF4742;
     border-radius: 6px;
     box-shadow: rgba(0, 0, 0, 0.1) 1px 2px 4px;
     box-sizing: border-box;
 }
 
 .queen {
-    background-image: linear-gradient(135deg, #f34080 40%, #fc894d);
-    color:gold;
+    padding: 0.6em 2em;
+    border: none;
+    outline: none;
+    color: gold;
+    background: #111;
+    cursor: pointer;
+    position: relative;
+    z-index: 0;
+    border-radius: 10px;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+}
+
+.queen:before {
+    content: "";
+    background: linear-gradient(45deg,
+            #ff0000,
+            #ff7300,
+            #fffb00,
+            #48ff00,
+            #00ffd5,
+            #002bff,
+            #7a00ff,
+            #ff00c8,
+            #ff0000);
+    position: absolute;
+    top: -2px;
+    left: -2px;
+    background-size: 400%;
+    z-index: -1;
+    filter: blur(5px);
+    -webkit-filter: blur(5px);
+    width: calc(100% + 4px);
+    height: calc(100% + 4px);
+    animation: glowing-queen 20s linear infinite;
+    transition: opacity 0.3s ease-in-out;
+    border-radius: 10px;
+}
+
+@keyframes glowing-queen {
+    0% {
+        background-position: 0 0;
+    }
+
+    50% {
+        background-position: 400% 0;
+    }
+
+    100% {
+        background-position: 0 0;
+    }
+}
+
+.queen:after {
+    z-index: -1;
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #222;
+    left: 0;
+    top: 0;
+    border-radius: 10px;
 }
 
 .worker {
-    background-image: linear-gradient(45deg, #FF512F 0%, #F09819 51%, #FF512F 100%);
+    background-color: transparent;
+    background-image: linear-gradient(to bottom, #fff, #f8eedb);
+    border: 0 solid #e5e7eb;
+    box-sizing: border-box;
+    color: #482307;
 }
 
 .scout {
-    background-image: linear-gradient(135deg, #FF4742 40%, #fc894d);
+    background-color: #c2fbd7;
+    color: green;
+    box-shadow: rgba(44, 187, 99, .2) 0 -25px 18px -14px inset, rgba(44, 187, 99, .15) 0 1px 2px, rgba(44, 187, 99, .15) 0 2px 4px, rgba(44, 187, 99, .15) 0 4px 8px, rgba(44, 187, 99, .15) 0 8px 16px, rgba(44, 187, 99, .15) 0 16px 32px;
 }
 .lastHit {
     background-image: none;
     background-color: red;
+    color:white;
+    font-weight: bold;
+}
+
+.isDead {
+    color:gray !important;
     font-weight: bold;
 }
 </style>
